bd62821b3fb9bb22882356fd3e66eff010760fb0
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-06-27 22:01:19 +0300

    complete lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,51 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    user = User.find_by(id: params[:user_id])
+    if user
+      items = user.items.includes(:user)
+      render json: items, include: :user
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
+  def show
+    user = User.find_by(id: params[:user_id])
+    if user
+      item = user.items.find_by(id: params[:id])
+      if item
+        render json: item, include: :user
+      else
+        render json: { error: "Item not found" }, status: :not_found
+      end
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      if params[:item].present?
+        item = user.items.build(item_params)
+        if item.save
+          render json: item, include: :user, status: :created
+        else
+          render json: { error: item.errors.full_messages }, status: :unprocessable_entity
+        end
+      else
+        render json: { error: "Item data not provided" }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
+  
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
